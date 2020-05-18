a7a7ee52245fe772a05bf1c04934ca17028a16bb
Renan Mendonça Tavares <renan.coding@gmail.com>
2020-05-18 16:39:04 -0300

    Gera modelo de super usando pop

---
+++ models/super.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/gobuffalo/pop/v5"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gofrs/uuid"
+	"time"
+)
+// Super is used by pop to map your supers database table to your go code.
+type Super struct {
+    ID uuid.UUID `json:"id" db:"id"`
+    CreatedAt time.Time `json:"created_at" db:"created_at"`
+    UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+// String is not required by pop and may be deleted
+func (s Super) String() string {
+	js, _ := json.Marshal(s)
+	return string(js)
+}
+
+// Supers is not required by pop and may be deleted
+type Supers []Super
+
+// String is not required by pop and may be deleted
+func (s Supers) String() string {
+	js, _ := json.Marshal(s)
+	return string(js)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (s *Super) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (s *Super) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (s *Super) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
