e24a4761720939ca7f280495b2c43a4eb8680b61
Renan Mendonça Tavares <renan.coding@gmail.com>
2020-05-20 15:58:23 -0300

    Atualiza struct de Super com informações

--- models/super.go
+++ models/super.go
@@ -2,16 +2,44 @@ package models
 
 import (
 	"encoding/json"
+	"time"
+
+	"github.com/gobuffalo/nulls"
 	"github.com/gobuffalo/pop/v5"
 	"github.com/gobuffalo/validate/v3"
 	"github.com/gofrs/uuid"
-	"time"
 )
+
 // Super is used by pop to map your supers database table to your go code.
 type Super struct {
-    ID uuid.UUID `json:"id" db:"id"`
-    CreatedAt time.Time `json:"created_at" db:"created_at"`
-    UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	ID              uuid.UUID    `json:"id" db:"id"`
+	CreatedAt       time.Time    `json:"created_at" db:"created_at"`
+	UpdatedAt       time.Time    `json:"updated_at" db:"updated_at"`
+	OriginalID      int          `json:"original_id" db:"original_id"`
+	Name            string       `json:"name" db:"name"`
+	FullName        nulls.String `json:"full_name" db:"full_name"`
+	PlaceOfBirth    nulls.String `json:"place_of_birth" db:"place_of_birth"`
+	FirstAppearance nulls.String `json:"first_appearance" db:"first_appearance"`
+	AlterEgos       string       `json:"alter_egos" db:"alter_egos"`
+	Publisher       string       `json:"publisher" db:"publisher"`
+	Alignment       string       `json:"alignment" db:"alignment"`
+	Gender          nulls.String `json:"gender" db:"gender"`
+	Race            nulls.String `json:"race" db:"race"`
+	HeightFeet      string       `json:"height_feet" db:"height_feet"`
+	HeightCm        int          `json:"height_cm" db:"height_cm"`
+	WeightLb        string       `json:"weight_lb" db:"weight_lb"`
+	WeightKg        int          `json:"weight_kg" db:"weight_kg"`
+	EyeColor        nulls.String `json:"eye_color" db:"eye_color"`
+	HairColor       nulls.String `json:"hair_color" db:"hair_color"`
+	Occupation      nulls.String `json:"occupation" db:"occupation"`
+	Base            nulls.String `json:"base" db:"base"`
+	Image           nulls.String `json:"image" db:"image"`
+	Intelligence    int          `json:"intelligence" db:"intelligence"`
+	Strength        int          `json:"strength" db:"strength"`
+	Speed           int          `json:"speed" db:"speed"`
+	Durability      int          `json:"durability" db:"durability"`
+	Power           int          `json:"power" db:"power"`
+	Combat          int          `json:"combat" db:"combat"`
 }
 
 // String is not required by pop and may be deleted
