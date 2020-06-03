fa208f7a25d9c814d7abcbf25e4e7e8952ee7774
Renan Mendonça Tavares <renan.coding@gmail.com>
2020-06-03 13:04:28 -0300

    Gera modelo de alias

---
+++ models/alias.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/gobuffalo/pop/v5"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gofrs/uuid"
+	"time"
+)
+// Alias is used by pop to map your aliases database table to your go code.
+type Alias struct {
+    ID uuid.UUID `json:"id" db:"id"`
+    CreatedAt time.Time `json:"created_at" db:"created_at"`
+    UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+// String is not required by pop and may be deleted
+func (a Alias) String() string {
+	ja, _ := json.Marshal(a)
+	return string(ja)
+}
+
+// aliases is not required by pop and may be deleted
+type aliases []Alias
+
+// String is not required by pop and may be deleted
+func (a aliases) String() string {
+	ja, _ := json.Marshal(a)
+	return string(ja)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (a *Alias) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (a *Alias) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (a *Alias) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
