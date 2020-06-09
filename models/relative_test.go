4da644865bf4923da1773c4e66f033e396e84cfd
Renan Mendonça Tavares <renan.coding@gmail.com>
2020-06-09 16:56:31 -0300

    Implementa teste de modelo de relative

--- models/relative_test.go
+++ models/relative_test.go
@@ -1,5 +1,12 @@
 package models
 
 func (ms *ModelSuite) Test_Relative() {
-	ms.Fail("This test needs to be implemented!")
+	super := &Super{}
+	relative := Relative{}
+	relative.Name = "Relative (Relation)"
+	super.Relatives = append(super.Relatives, relative)
+	_, err := ms.DB.ValidateAndCreate(super)
+	if err != nil {
+		ms.Fail("Erro ao validar e criar super")
+	}
 }
