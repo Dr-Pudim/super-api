a9823718d78d7a3d002dfa73a4dc6dd47ee5bf3c
Renan Mendonça Tavares <renan.coding@gmail.com>
2020-06-09 16:59:20 -0300

    Muda mensagem de falha de teste de modelo de relative

--- models/relative_test.go
+++ models/relative_test.go
@@ -7,6 +7,6 @@ func (ms *ModelSuite) Test_Relative() {
 	super.Relatives = append(super.Relatives, relative)
 	_, err := ms.DB.ValidateAndCreate(super)
 	if err != nil {
-		ms.Fail("Erro ao validar e criar super")
+		ms.Fail("Erro ao validar e criar super com relative")
 	}
 }
